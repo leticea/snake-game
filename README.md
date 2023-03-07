b1a8ea3903875690319229cb19ed4ccb2c72570e
leticea <nascimento.leticeah@gmail.com>
2023-03-07 11:43:13 -0300

    editando o readme

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 <br>
 
 <p align="center">
-  <img alt="snake" src=".github/image.png">
+  <img alt="" src=".github/image.png">
 </p>
 
 ## Snake Game
@@ -17,7 +17,7 @@
 
 Esse projeto foi desenvolvido com as seguintes tecnologias:
 
-- [HTML](https://developer.mozilla.org/pt-BR/docs/Web/HTML) 
+- [HTML5](https://developer.mozilla.org/pt-BR/docs/Web/HTML)
 - [JavaScript](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
 
 ## 💻 Projeto
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 class Snake {
   constructor(x, y, size) {
-    this.x = x
-    this.y = y
+    this.x = x;
+    this.y = y;
     this.size = size;
-    this.tail = [{x:this.x, y:this.y}]
-    this.rotateX = 0
-    this.rotateY = 1
+    this.tail = [{ x: this.x, y: this.y }];
+    this.rotateX = 0;
+    this.rotateY = 1;
   }
 
   move() {
@@ -13,26 +13,23 @@ class Snake {
     if (this.rotateX == 1) {
       newRect = {
         x: this.tail[this.tail.length - 1].x + this.size,
-        y: this.tail[this.tail.length - 1].y
-      }
-
+        y: this.tail[this.tail.length - 1].y,
+      };
     } else if (this.rotateX == -1) {
       newRect = {
         x: this.tail[this.tail.length - 1].x - this.size,
-        y: this.tail[this.tail.length - 1].y
-      }
-
+        y: this.tail[this.tail.length - 1].y,
+      };
     } else if (this.rotateY == 1) {
       newRect = {
         x: this.tail[this.tail.length - 1].x,
-        y: this.tail[this.tail.length - 1].y + this.size
-      }
-
+        y: this.tail[this.tail.length - 1].y + this.size,
+      };
     } else if (this.rotateY == -1) {
       newRect = {
         x: this.tail[this.tail.length - 1].x,
-        y: this.tail[this.tail.length - 1].y - this.size
-      }
+        y: this.tail[this.tail.length - 1].y - this.size,
+      };
     }
 
     this.tail.shift();
@@ -43,11 +40,13 @@ class Snake {
 class Apple {
   constructor() {
     let isTouching;
-    
-    while(true) {
+
+    while (true) {
       isTouching = false;
-      this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size;
-      this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size;
+      this.x =
+        Math.floor((Math.random() * canvas.width) / snake.size) * snake.size;
+      this.y =
+        Math.floor((Math.random() * canvas.height) / snake.size) * snake.size;
 
       for (let i = 0; i < snake.tail.length; i++) {
         if (this.x == snake.tail[i].x && this.y == snake.tail[i].y) {
@@ -60,22 +59,22 @@ class Apple {
 
       if (!isTouching) {
         break;
-      }      
+      }
     }
   }
 }
 
-let canvas = document.getElementById('canvas');
+let canvas = document.getElementById("canvas");
 let snake = new Snake(20, 20, 20);
 let apple = new Apple();
-let canvasContext = canvas.getContext('2d');
+let canvasContext = canvas.getContext("2d");
 
 window.onload = () => {
   gameLoop();
-}
+};
 
 function gameLoop() {
-  setInterval(show, 1000/15);
+  setInterval(show, 1000 / 15);
 }
 
 function show() {
@@ -92,31 +91,28 @@ function update() {
 }
 
 function eatApple() {
-  if (snake.tail[snake.tail.length - 1].x == apple.x &&
-      snake.tail[snake.tail.length - 1].y == apple.y) {
-        snake.tail[snake.tail.length] = {x: apple.x, y: apple.y}
-        apple = new Apple()
-      }
+  if (
+    snake.tail[snake.tail.length - 1].x == apple.x &&
+    snake.tail[snake.tail.length - 1].y == apple.y
+  ) {
+    snake.tail[snake.tail.length] = { x: apple.x, y: apple.y };
+    apple = new Apple();
+  }
 }
 
 function checkHitWall() {
-  let headTail = snake.tail[snake.tail.length - 1]
+  let headTail = snake.tail[snake.tail.length - 1];
 
-  if (headTail.x == - snake.size) {
+  if (headTail.x == -snake.size) {
     headTail.x = canvas.width - snake.size;
-
-  } else if (headTail.x == - canvas.width) {
+  } else if (headTail.x == -canvas.width) {
     headTail.x = 0;
-
-  } else if (headTail.y == - snake.size) {
+  } else if (headTail.y == -snake.size) {
     headTail.y = canvas.height - snake.size;
-
   } else if (headTail.y == canvas.height) {
     headTail.y = 0;
   }
 }
-
-
 
 function draw() {
   createRect(0, 0, canvas.width, canvas.height, "black");
@@ -127,13 +123,18 @@ function draw() {
       snake.tail[i].x + 2.5,
       snake.tail[i].y + 2.5,
       snake.size - 5,
-      snake.size - 5, 'white'
-    )
+      snake.size - 5,
+      "white"
+    );
   }
 
   canvasContext.font = "20px Arial";
   canvasContext.fillStyle = "#00FF42";
-  canvasContext.fillText("Score: " + (snake.tail.length - 1), canvas.width - 120, 18);
+  canvasContext.fillText(
+    "Score: " + (snake.tail.length - 1),
+    canvas.width - 120,
+    18
+  );
   createRect(apple.x, apple.y, apple.size, apple.size, apple.color);
 }
 
@@ -147,20 +148,15 @@ window.addEventListener("keydown", (event) => {
     if (event.keyCode == 37 && snake.rotateX != 1) {
       snake.rotateX = -1;
       snake.rotateY = 0;
-
     } else if (event.keyCode == 38 && snake.rotateY != 1) {
       snake.rotateX = 0;
       snake.rotateY = -1;
-
     } else if (event.keyCode == 39 && snake.rotateX != -1) {
       snake.rotateX = 1;
       snake.rotateY = 0;
-
     } else if (event.keyCode == 40 && snake.rotateY != -1) {
       snake.rotateX = 0;
       snake.rotateY = 1;
     }
-  }, 1)
-})
-
-
+  }, 1);
+});
